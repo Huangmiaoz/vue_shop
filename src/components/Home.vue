5e0c985023f6d4d8b1806c1e54bb517fe332a285
Huangmiaoz <1873151053@qq.com>
2022-08-21 12:04:36 +0800

    menu

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,18 +1,113 @@
 <template>
-  <div>
-    <!-- <el-button type="info">退出</el-button> -->
-  </div>
+  <el-container class="home-container">
+    <el-header>
+      <span>后台管理系统</span>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <el-container>
+      <el-aside :width="isCollapse ? '64px' : '200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          active-text-color="#409Eff"
+          background-color="#333744"
+          text-color="#fff"  
+          unique-opened ="true"
+          :collapse = "isCollapse"
+          collapse-transition = "false"
+        >
+          <el-sub-menu :index="item.id + ''" v-for="item in muneList" :key="item.id">
+            <template #title text-color=#fff>
+              <!-- <el-icon><location /></el-icon> -->
+              <i :class="iconsObj[item.id]"></i>
+              <span>{{item.authName}}</span>
+            </template>
+             <el-menu-item :index="subItem.id + ''" v-for="subItem in item.children" :key="subItem.id">
+             
+              <template #title text-color=#fff><el-icon><Grid /></el-icon>{{subItem.authName}}</template>
+            </el-menu-item>
+          </el-sub-menu>
+        </el-menu>
+      </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+
 </template>
 
 <script>
+
 export default {
+  data() {
+    return {
+      muneList : [],
+      iconsObj : {
+        '125' : 'iconfont icon-user',
+        '103' : 'iconfont icon-tijikongjian',
+        '101' : 'iconfont icon-shangpin',
+        '102' : 'iconfont icon-danju',
+        '145' : 'iconfont icon-baobiao'
+      },
+      isCollapse : false
+    }
+  },
+  created() {
+    this.getMuneList();
+  },
   methods : {
     logout() {
       window.sessionStorage.clear();
       this.$router.push('/login');
+    },
+    async getMuneList() {
+      const res = await this.$http.get('menus');
+      console.log(res);
+      if( res.data.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.muneList = res.data.data;
+    },
+    // 菜单的折叠和展开
+    toggleCollapse() {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  background-color: #373D41;
+  display: flex;
+  justify-content: space-between;
+  // 竖直方向居中
+  align-items: center;
+  // 字体颜色
+  color: #fff;
+  font-size: 20px;
+
+}
+.el-aside {
+  background-color: #333744;
+  .el-menu {
+    // 不然菜单展开时边框会对不齐
+    border-right: none;
+  }
+}
+.el-container {
+  background-color: #eaedf1;
+}
+.iconfont {
+  margin-right: 10px;
+}
+.toggle-button {
+  background-color: #4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  // 鼠标移上去是小手的形式
+  cursor: pointer; 
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -4,22 +4,36 @@ import router from './router'
 import store from './store'
 
 import ElementPlus from 'element-plus'
-import { Message } from 'element-plus'
+import { ElMessage  } from 'element-plus'
 import 'element-plus/dist/index.css'
-
 // 导入全局样式表
 import './assets/css/global.css'
 // 导入字体图标
 import './assets/fonts/iconfont.css'
+
 
 import axios from 'axios'
 
 const app = createApp(App);
 // 配置请求的跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+// 请求的拦截器
+axios.interceptors.request.use(config => {
+  console.log(config);
+  config.headers.Authorization = window.sessionStorage.getItem('token');
+  return config;
+})
 app.config.globalProperties.$http = axios
 
 // $message
-app.config.globalProperties.$message = Message
+app.config.globalProperties.$message = ElMessage
 
 app.use(store).use(ElementPlus).use(router).mount('#app')
+
+// 统一导入el-icon图标
+import * as ElIconModules from '@element-plus/icons-vue'
+
+// 统一注册el-icon图标
+for(let iconName in ElIconModules){
+  app.component(iconName,ElIconModules[iconName])
+}
