35b63df01d48fbae51754dd3c7517c1ad12cfd34
Huangmiaoz <1873151053@qq.com>
2022-08-26 11:37:14 +0800

    complete order

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,7 @@ import Cate from '../components/goods/Cate.vue'
 import Params from '../components/goods/params.vue'
 import List from '../components/goods/List.vue';
 import Add from '../components/goods/Add.vue';
+import Order from '../components/order/Order.vue';
 
 const routes = [
   {path : '/', redirect : 'login'},
@@ -26,6 +27,7 @@ const routes = [
       { path : '/params', component : Params},
       { path : '/goods', component : List},
       { path : '/goods/add', component : Add},
+      { path : '/orders', component : Order},
     ]
   },
 ]
